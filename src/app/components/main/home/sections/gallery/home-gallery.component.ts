f2a7cc0d74ef5684c8e53fe33f76a39a4838feb8
MenomDE <menom@berlin.de>
2026-07-07 20:49:57 +0200

    feat(home): replace gallery galleria with carousel

--- src/app/components/main/home/sections/gallery/home-gallery.component.ts
+++ src/app/components/main/home/sections/gallery/home-gallery.component.ts
@@ -1,15 +1,20 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { GalleriaModule } from 'primeng/galleria';
+import { CarouselModule } from 'primeng/carousel';
 import { HomeGalleryImage } from '../../../../../shared/types/home.types';
 
 @Component({
     selector: 'ttt-home-gallery',
     standalone: true,
-    imports: [GalleriaModule],
+    imports: [CarouselModule],
     templateUrl: './home-gallery.component.html',
     styleUrl: './home-gallery.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeGalleryComponent {
     galleryImages = input.required<HomeGalleryImage[]>();
+
+    readonly responsiveOptions = [
+        { breakpoint: '1024px', numVisible: 2, numScroll: 1 },
+        { breakpoint: '640px', numVisible: 1, numScroll: 1 },
+    ];
 }
