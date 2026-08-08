16beb2858f1efe6198f7c8946a09b391855727bd
MenomDE <menom@berlin.de>
2026-08-08 16:21:34 +0200

    chore: edit meta descr, add TTT-Logo

--- src/app/components/main/home/sections/branches/home-branches.component.ts
+++ src/app/components/main/home/sections/branches/home-branches.component.ts
@@ -2,13 +2,12 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonDirective } from '@openng/optimus-ui/button';
 import { HomeBranch, HomeCommunityStat } from '../../../../../shared/types/home.types';
-import { SectionHeaderComponent } from '../../../../../shared/components/section-header/section-header.component';
 import { TTT_LINKS } from '../../../../../shared/constants/external-links';
 
 @Component({
     selector: 'ttt-home-branches',
     standalone: true,
-    imports: [RouterLink, ButtonDirective, SectionHeaderComponent],
+    imports: [RouterLink, ButtonDirective],
     templateUrl: './home-branches.component.html',
     styleUrl: './home-branches.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
